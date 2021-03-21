6d12338faca43576ee6979d65da418e8941f2d27
brycx <brycx@protonmail.com>
2021-03-21 08:41:28 +0100

    b64: Use alloc

--- src/version2.rs
+++ src/version2.rs
@@ -8,20 +8,24 @@ use crate::pae;
 use ct_codecs::{Base64UrlSafeNoPadding, Decoder, Encoder};
 use rand_core::{CryptoRng, RngCore};
 
-fn encode_b64<'a, T: AsRef<[u8]>>(encoded: T) -> Result<&'a str, Errors> {
+fn encode_b64<'a, T: AsRef<[u8]>>(encoded: T) -> Result<String, Errors> {
     let inlen = encoded.as_ref().len();
     let mut buf = vec![0u8; Base64UrlSafeNoPadding::encoded_len(inlen)?];
 
-    Ok(Base64UrlSafeNoPadding::encode_to_str(&mut buf, encoded)?)
+    let ret: String = Base64UrlSafeNoPadding::encode_to_str(&mut buf, encoded)?.into();
+
+    Ok(ret)
 }
 
-fn decode_b64<'a, T: AsRef<[u8]>>(encoded: T) -> Result<&'a [u8], Errors> {
+fn decode_b64<'a, T: AsRef<[u8]>>(encoded: T) -> Result<Vec<u8>, Errors> {
     let inlen = encoded.as_ref().len();
     // We can use encoded len here, even if it returns more than needed,
     // because ct-codecs allows this.
     let mut buf = vec![0u8; Base64UrlSafeNoPadding::encoded_len(inlen)?];
 
-    Ok(Base64UrlSafeNoPadding::decode(&mut buf, encoded, None)?)
+    let ret: Vec<u8> = Base64UrlSafeNoPadding::decode(&mut buf, encoded, None)?.into();
+
+    Ok(ret)
 }
 
 /// Validate that a token begins with a given header.purpose and does not contain more than:
