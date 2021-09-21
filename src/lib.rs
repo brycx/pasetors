ef72d978b58e6423783fdb8e0ce1250b0474ea26
brycx <brycx@protonmail.com>
2021-09-21 12:38:52 +0200

    Add top-level API that interacts directly with Claims and ClaimsValidationRules

--- src/lib.rs
+++ src/lib.rs
@@ -52,3 +52,83 @@ pub mod keys;
 pub mod version2;
 /// PASETO version 4 tokens.
 pub mod version4;
+
+/// PASETO public tokens using `Claims`.
+pub mod public {
+    use crate::claims::{Claims, ClaimsValidationRules};
+    use crate::errors::Errors;
+    use crate::keys::{AsymmetricPublicKey, AsymmetricSecretKey};
+
+    /// Create a public token using the latest PASETO version (v4).
+    pub fn sign(
+        secret_key: &AsymmetricSecretKey,
+        public_key: &AsymmetricPublicKey,
+        message: &Claims,
+        footer: Option<&[u8]>,
+        implicit_assert: Option<&[u8]>,
+    ) -> Result<String, Errors> {
+        crate::version4::PublicToken::sign(
+            secret_key,
+            public_key,
+            message.to_string().unwrap().as_bytes(),
+            footer,
+            implicit_assert,
+        )
+    }
+
+    /// Verify a public token using the latest PASETO version (v4). If verification passes, validate the claims according to the
+    /// `validation_rules`.
+    pub fn verify(
+        public_key: &AsymmetricPublicKey,
+        token: &str,
+        validation_rules: &ClaimsValidationRules,
+        footer: Option<&[u8]>,
+        implicit_assert: Option<&[u8]>,
+    ) -> Result<Claims, Errors> {
+        crate::version4::PublicToken::verify(public_key, token, footer, implicit_assert)?;
+
+        let claims = Claims::from_string(token)?;
+        validation_rules.validate_claims(&claims)?;
+
+        Ok(claims)
+    }
+}
+
+/// PASETO local tokens using `Claims`.
+pub mod local {
+    use crate::claims::{Claims, ClaimsValidationRules};
+    use crate::errors::Errors;
+    use crate::keys::SymmetricKey;
+
+    /// Create a local token using the latest PASETO version (v4).
+    pub fn encrypt(
+        secret_key: &SymmetricKey,
+        message: &Claims,
+        footer: Option<&[u8]>,
+        implicit_assert: Option<&[u8]>,
+    ) -> Result<String, Errors> {
+        crate::version4::LocalToken::encrypt(
+            secret_key,
+            message.to_string().unwrap().as_bytes(),
+            footer,
+            implicit_assert,
+        )
+    }
+
+    /// Verify a local token using the latest PASETO version (v4). If verification passes, validate the claims according to the
+    /// `validation_rules`.
+    pub fn decrypt(
+        secret_key: &SymmetricKey,
+        token: &str,
+        validation_rules: &ClaimsValidationRules,
+        footer: Option<&[u8]>,
+        implicit_assert: Option<&[u8]>,
+    ) -> Result<Claims, Errors> {
+        let raw_payload =
+            crate::version4::LocalToken::decrypt(secret_key, token, footer, implicit_assert)?;
+        let claims = Claims::from_bytes(&raw_payload)?;
+        validation_rules.validate_claims(&claims)?;
+
+        Ok(claims)
+    }
+}
