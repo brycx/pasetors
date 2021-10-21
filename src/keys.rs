e5904c72262aad620357ad370af67ce75f240d58
brycx <brycx@protonmail.com>
2021-10-21 19:49:56 +0200

    keys: Add missing docs and NIT refactorings

--- src/keys.rs
+++ src/keys.rs
@@ -3,15 +3,20 @@ use alloc::vec::Vec;
 use core::fmt::Debug;
 use std::marker::PhantomData;
 
+/// Version 2 of the PASETO spec.
 pub struct V2;
+/// Version 4 of the PASETO spec.
 pub struct V4;
 
 mod private {
-    // since this trait is in a private module it
-    // cannot be implemented from other crates
+    // Since this trait is in a private module it
+    // cannot be implemented from other crates.
+    // This prevents users from implementing this trait themselves,
+    // possibly defining wrong keys.
     pub trait PrivateTrait {}
 }
 
+/// A marker-trait for a key that is either [`V2`] or [`V4`].
 pub trait V2orV4: private::PrivateTrait {}
 impl V2orV4 for V2 {}
 impl V2orV4 for V4 {}
@@ -21,7 +26,7 @@ impl private::PrivateTrait for V4 {}
 const V2_KEYSIZE: usize = 32;
 const V4_KEYSIZE: usize = V2_KEYSIZE;
 
-/// A symmetric key used for `.local` tokens.
+/// A symmetric key used for `.local` tokens, given a version `V`.
 pub struct SymmetricKey<V> {
     bytes: Vec<u8>,
     phantom: PhantomData<V>,
@@ -41,31 +46,27 @@ impl<V> Drop for SymmetricKey<V> {
 }
 
 impl<V: V2orV4> SymmetricKey<V> {
-    /// Randomly generate a `SymmetricKey` for a `version`.
+    /// Randomly generate a `SymmetricKey`.
     pub fn gen() -> Result<Self, Errors> {
         let mut rng_bytes = vec![0u8; V4_KEYSIZE];
         getrandom::getrandom(&mut rng_bytes)?;
 
-        return Ok(Self {
+        Ok(Self {
             bytes: rng_bytes,
             phantom: PhantomData,
-        });
-
-        Err(Errors::KeyError)
+        })
     }
 
-    /// Create a `SymmetricKey` from `bytes`, to be used with `version`.
+    /// Create a `SymmetricKey` from `bytes`.
     pub fn from(bytes: &[u8]) -> Result<Self, Errors> {
         if bytes.len() != V4_KEYSIZE {
             return Err(Errors::KeyError);
         }
 
-        return Ok(Self {
+        Ok(Self {
             bytes: bytes.to_vec(),
             phantom: PhantomData,
-        });
-
-        Err(Errors::KeyError)
+        })
     }
 
     /// Return this as a byte-slice.
@@ -74,7 +75,7 @@ impl<V: V2orV4> SymmetricKey<V> {
     }
 }
 
-/// An asymmetric secret key used for `.public` tokens.
+/// An asymmetric secret key used for `.public` tokens, given a version `V`.
 pub struct AsymmetricSecretKey<V> {
     bytes: Vec<u8>,
     phantom: PhantomData<V>,
@@ -94,18 +95,16 @@ impl<V> Drop for AsymmetricSecretKey<V> {
 }
 
 impl<V: V2orV4> AsymmetricSecretKey<V> {
-    /// Create an `AsymmetricSecretKey` from `bytes`, to be used with `version`.
+    /// Create an `AsymmetricSecretKey` from `bytes`.
     pub fn from(bytes: &[u8]) -> Result<Self, Errors> {
         if bytes.len() != ed25519_dalek::SECRET_KEY_LENGTH {
             return Err(Errors::KeyError);
         }
 
-        return Ok(Self {
+        Ok(Self {
             bytes: bytes.to_vec(),
             phantom: PhantomData,
-        });
-
-        Err(Errors::KeyError)
+        })
     }
 
     /// Return this as a byte-slice.
@@ -115,25 +114,23 @@ impl<V: V2orV4> AsymmetricSecretKey<V> {
 }
 
 #[derive(Debug)]
-/// An asymmetric public key used for `.public` tokens.
+/// An asymmetric public key used for `.public` tokens, given a version `V`.
 pub struct AsymmetricPublicKey<V> {
     bytes: Vec<u8>,
     phantom: PhantomData<V>,
 }
 
 impl<V: V2orV4> AsymmetricPublicKey<V> {
-    /// Create an `AsymmetricPublicKey` from `bytes`, to be used with `version`.
+    /// Create an `AsymmetricPublicKey` from `bytes`.
     pub fn from(bytes: &[u8]) -> Result<Self, Errors> {
         if bytes.len() != ed25519_dalek::PUBLIC_KEY_LENGTH {
             return Err(Errors::KeyError);
         }
 
-        return Ok(Self {
+        Ok(Self {
             bytes: bytes.to_vec(),
             phantom: PhantomData,
-        });
-
-        Err(Errors::KeyError)
+        })
     }
 
     /// Return this as a byte-slice.
